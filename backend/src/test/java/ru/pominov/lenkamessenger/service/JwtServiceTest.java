91c9234cd40465b3a4d18f79a497780f5d41fead
NikitaBuffy <pominov.nk@gmail.com>
2024-12-20 09:37:20 +0300

    #52 test(backend): add unit tests for JwtService

---
+++ backend/src/test/java/ru/pominov/lenkamessenger/service/JwtServiceTest.java
@@ -0,0 +1,30 @@
+package ru.pominov.lenkamessenger.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import ru.pominov.lenkamessenger.model.user.Role;
+import ru.pominov.lenkamessenger.model.user.User;
+import ru.pominov.lenkamessenger.service.jwt.JwtService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(properties = {"token.signing.key=SWD1mQ+vpshov3M63EIe6d8Thyf0AJ1toqSR32wZpPo="})
+public class JwtServiceTest {
+
+    @Autowired
+    private JwtService jwtService;
+
+    User user = new User(1L, "testuser123", "password123", "test@test.ru", "John", "Doe", Role.ROLE_USER);
+
+    @Test
+    public void shouldGenerateAndCheckToken() {
+        String token = jwtService.generateToken(user);
+
+        assertNotNull(token);
+        assertEquals("testuser123", jwtService.extractUsername(token));
+        assertTrue(jwtService.isTokenValid(token, user));
+    }
+}
